3f1c2406678d9d4c83f55455a63f0360aed50ab1
dongyaolin <xensedyl@126.com>
2026-06-24 21:35:45 +0800

    Remove FrankaResearch3Dexhand from __all__ exports in the init file, keeping only FrankaResearch3DexhandConfig.

--- lerobot_robot_franka_research3_dexhand/__init__.py
+++ lerobot_robot_franka_research3_dexhand/__init__.py
@@ -1,4 +1,3 @@
 from .config_franka_research3_dexhand import FrankaResearch3DexhandConfig
-from .franka_research3_dexhand import FrankaResearch3Dexhand
 
-__all__ = ["FrankaResearch3Dexhand", "FrankaResearch3DexhandConfig"]
+__all__ = ["FrankaResearch3DexhandConfig"]
